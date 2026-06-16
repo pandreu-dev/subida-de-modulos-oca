675eaf606758b69231bf34f18fc124101090af07
Pablo Andreu <pandreu@aunnait.es>
2026-06-16 15:30:09 +0200

    cambio para evitar el errror

--- project_delivery_analytic_valuation/__manifest__.py
+++ project_delivery_analytic_valuation/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Project Delivery Analytic Valuation",
     "summary": "Tracks manual delivery inventory costs on project dashboards",
-    "version": "19.0.1.1.3",
+    "version": "19.0.1.1.4",
     "category": "Inventory/Inventory",
     "author": "Aunnna",
     "license": "LGPL-3",
