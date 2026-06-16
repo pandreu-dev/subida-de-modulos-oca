65e72736f2765803db628b16d2acee5b80b8422e
Pablo Andreu <pandreu@aunnait.es>
2026-06-16 08:14:44 +0200

    nuew etiquetas

--- aunna_product_labels/__manifest__.py
+++ aunna_product_labels/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Aunnna Product Labels",
     "summary": "Anade formatos personalizados de etiquetas al asistente estandar de productos",
-    "version": "19.0.1.1.0",
+    "version": "19.0.1.2.0",
     "category": "Inventory/Inventory",
     "author": "Aunnna",
     "license": "LGPL-3",
