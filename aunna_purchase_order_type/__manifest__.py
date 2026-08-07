f05ba503c0480f6e748b5e00b0a9b592aae37f24
Pablo Andreu <pandreu@aunnait.es>
2026-08-07 12:11:16 +0200

    añadir a la view correctya de pedido de compra el filtrar tipo pedido agrupar

--- aunna_purchase_order_type/__manifest__.py
+++ aunna_purchase_order_type/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Aunnna Purchase Order Type",
     "summary": "Anade tipo de pedido en solicitudes de presupuesto y pedidos de compra",
-    "version": "19.0.1.1.1",
+    "version": "19.0.1.1.2",
     "category": "Purchase",
     "author": "Aunnna",
     "license": "LGPL-3",
