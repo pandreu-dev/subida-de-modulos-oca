f6aed527b4a670c12b5476f1f90ca5947c4ea5b8
Pablo Andreu <pandreu@aunnait.es>
2026-08-12 12:57:22 +0200

    cambios y subidos

---
+++ zambudio_hr_leave_ui/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "Zambudio - Ausencias: boton cancelar y lista de calendario",
+    "summary": "Visibilidad del boton Cancelar en ausencias y columnas del listado del calendario de ausencias",
+    "version": "19.0.1.0.0",
+    "category": "Human Resources/Time Off",
+    "author": "Zambudio",
+    "license": "LGPL-3",
+    "depends": [
+        "hr_holidays",
+    ],
+    "data": [
+        "views/hr_leave_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
