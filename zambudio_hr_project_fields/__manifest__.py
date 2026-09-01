0dc9c21531abaa7e4a10fd79266da5d4f048a1fe
Pablo Andreu <pandreu@aunnait.es>
2026-09-01 12:59:08 +0200

    tipo empleoado y sub

--- zambudio_hr_project_fields/__manifest__.py
+++ zambudio_hr_project_fields/__manifest__.py
@@ -1,7 +1,7 @@
 {
     "name": "Zambudio - Campos de empleado y proyecto (Studio a codigo)",
     "summary": "Adopta como codigo los campos x_studio_ de hr.employee y la Productividad de project.project",
-    "version": "19.0.1.0.0",
+    "version": "19.0.1.1.0",
     "category": "Human Resources",
     "author": "Zambudio",
     "license": "LGPL-3",
@@ -9,6 +9,9 @@
         "hr",
         "project",
         "zambudio_master_data",
+    ],
+    "data": [
+        "views/hr_employee_views.xml",
     ],
     # Adopcion (state=base + propiedad del modulo) ANTES de reflejar, en instalacion nueva.
     "pre_init_hook": "pre_init_hook",
